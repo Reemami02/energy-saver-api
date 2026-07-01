4e03340e2cf0e6ff14a6153063178ac4b6e0ca29
Reemami02 <reem.ami002@gmail.com>
2026-07-01 08:52:15 +0300

    Delete src directory

--- src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const chatController = require("../controllers/chat.controller");
-
-router.post("/", chatController.chat);
-
-module.exports = router;
