bdebf1d2b5b05f7133cc38fa48dcd5dc179a51b5
Reemami02 <reem.ami002@gmail.com>
2026-07-01 08:44:51 +0300

    Enhance sendToPredictAI with token and logging
    Updated sendToPredictAI to include token authorization and added logging for payload and response.

--- src/services/aiServer.js
+++ src/services/aiServer.js
@@ -4,14 +4,23 @@ const { mapDevicePayload } = require("./ai.adapter");
 // ==========================
 // PREDICT API
 // ==========================
-exports.sendToPredictAI = async (device, reading) => {
+exports.sendToPredictAI = async (device, reading, token) => {
   try {
-    const payload = mapDevicePayload(device, reading);
+    const payload = mapDevicePayload(device, reading , token);
 
     const response = await axios.post(
       "http://64.225.101.222:8000/predict",
-      payload
+      payload,
+      {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
     );
+    console.log("========== PAYLOAD TO AI ==========");
+    console.log(JSON.stringify(payload, null, 2));
+    console.log("========== AI RESPONSE ==========");
+console.log(response.data);
 
     return response.data;
   } catch (error) {
